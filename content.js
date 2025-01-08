451541b826cb4dfaf6a4de8a4cc9092ca9085de5
Alexey Grishin <95340036+Nifacy@users.noreply.github.com>
2025-01-09 02:17:07 +0300

    Add label elements (#2)

--- content.js
+++ content.js
@@ -1,4 +1,35 @@
-console.log("Start");
+/* Constants */
+
+const SUGGESTED_LABELS_CONTAINER_ID = "suggested-labels-container";
+
+/* Global state */
+
+let suggestedLabels = ["bug", "documentation"];
+
+/* Labels module */
+
+function __createLabelItem(name, callback) {
+    const element = document.createElement("div");
+    element.setAttribute("class", "suggested-labels__item");
+    element.innerText = name;
+    element.onclick = () => callback(name);
+
+    return element;
+}
+
+function createSuggestedLabelsContainer(names, callback) {
+    const suggestedLabelsContainer = document.createElement("div");
+    suggestedLabelsContainer.setAttribute("class", "suggested-labels");
+
+    for (const name of names) {
+        const item = __createLabelItem(name, callback);
+        suggestedLabelsContainer.appendChild(item);
+    }
+
+    return suggestedLabelsContainer;
+}
+
+/* Main */
 
 function getIssueData() {
     const issueTitle = document.getElementById('issue_title')?.value || '';
@@ -9,6 +40,52 @@ function getIssueData() {
     };
 }
 
+/* Labels */
+
+function getLabelMenuItems() {
+    let menuItems = [];
+    const labelMenuItems = document.querySelectorAll("#labels-select-menu .js-filterable-issue-labels .select-menu-item");
+
+    for (const menuItem of labelMenuItems) {
+        menuItems.push({
+            name: menuItem.getAttribute("data-prio-filter-value"),
+            isSelected: menuItem.getAttribute("aria-checked") === "true",
+            element: menuItem,
+        });
+    }
+
+    return menuItems;
+}
+
+function getActiveLabels() {
+    let selectedLabels = [];
+
+    for (const menuItem of getLabelMenuItems()) {
+        if (menuItem.isSelected) {
+            selectedLabels.push(menuItem.name);
+        }
+    }
+
+    return selectedLabels;
+}
+
+function selectLabel(name) {
+    if (getActiveLabels().includes(name)) {
+        return;
+    }
+
+    const selectMenu = document.querySelector("#labels-select-menu summary");
+
+    for (const menuItem of getLabelMenuItems()) {
+        if (menuItem.name === name) {
+            menuItem.element.click();
+            selectMenu.click();
+            selectMenu.click();
+            return;
+        }
+    }
+}
+
 function handleInputChange() {
     const issueData = getIssueData();
     console.log("Issue Data:", issueData);
@@ -16,11 +93,11 @@ function handleInputChange() {
 
 function debounce(func, delay) {
     let timeoutId;
-    return function(...args) {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => func.apply(this, args), delay);
+    return function (...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => func.apply(this, args), delay);
     };
-  }  
+}
 
 function setupInputListeners() {
     console.log("Setup input listeners...");
@@ -40,9 +117,51 @@ function setupInputListeners() {
     console.log("Setup input listeners... [ok]");
 }
 
+function getSuggestedNotActiveLabels() {
+    const activeLabels = getActiveLabels();
+    let result = [];
+
+    for (const label of suggestedLabels) {
+        if (!activeLabels.includes(label)) {
+            result.push(label);
+        }
+    }
+
+    return result;
+}
+
+function insertContainer(container) {
+    const labelsSidebar = document.querySelector('.js-issue-sidebar-form[data-cache-name="labels"]');
+    const lastElement = labelsSidebar.children[labelsSidebar.children.length - 1];
+    labelsSidebar.insertBefore(container, lastElement);
+}
+
+function initSuggestedLabelsContainer() {
+    const labels = getSuggestedNotActiveLabels();
+    const container = createSuggestedLabelsContainer(labels, selectLabel);
+    container.id = SUGGESTED_LABELS_CONTAINER_ID;
+    insertContainer(container);
+}
+
+const callback = (mutationsList, observer) => {
+    if (document.getElementById(SUGGESTED_LABELS_CONTAINER_ID)) return;
+    console.log("Container was deleted!");
+    initSuggestedLabelsContainer();
+};
+
+console.log("Start");
+
 window.addEventListener("load", () => {
     console.log("Page loaded");
     console.log("Setup ...");
     setupInputListeners();
     console.log("Setup ... [ok]");
+
+    console.log("Init container ...");
+    initSuggestedLabelsContainer();
+
+    const observer = new MutationObserver(callback);
+    const labelsSidebar = document.querySelector('#new_issue');
+    observer.observe(labelsSidebar, { childList: true, subtree: true });
+    console.log("Init container ... [ok]");
 });
